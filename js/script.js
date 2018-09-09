9958b369b7d651f8cdbef5f91aa51255073952d6
amandamoeller <36225710+amandamoeller@users.noreply.github.com>
2018-09-09 18:42:33 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,17 +16,61 @@ window.onload = function() {
 
 
 
+
 var imget = document.getElementById("kvadratet");
 var sanget = new Audio('../sound/rummetfyldt.mp3');
 
-imget.addEventListener("mouseover", playMusic, false);
-imget.addEventListener("mouseout", stopMusic, false);
-
-function playMusic() {
+imget.addEventListener('mouseover', function(e) {
     sanget.play();
-}
+})
 
-function stopMusic () {
+imget.addEventListener('mouseout', function(e) {
     sanget.pause();
-}
+})
+
+
+
+var imgto = document.getElementById("kvadratto");
+var sangto = new Audio('../sound/scream.mp3');
+
+imgto.addEventListener('mouseover', function(e) {
+    sangto.play();
+})
+
+imgto.addEventListener('mouseout', function(e) {
+    sangto.pause();
+})
+
+
+
+
+
+var imgtre = document.getElementById("kvadrattre");
+var sangtre = new Audio('../sound/christmas.mp3');
+
+imgtre.addEventListener('mouseover', function(e) {
+    sangtre.play();
+})
+
+imgtre.addEventListener('mouseout', function(e) {
+    sangtre.pause();
+})
+
+
+
+
+
+var imgfire = document.getElementById("kvadratfire");
+var sangfire = new Audio('../sound/theend.mp3');
+
+imgfire.addEventListener('mouseover', function(e) {
+    sangfire.play();
+})
+
+imgfire.addEventListener('mouseout', function(e) {
+    sangfire.pause();
+})
+
+
+
 
